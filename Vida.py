5a414079f5984148e986a0a23ebc9ef243e5c7e3
NatooLex <55214351+NatooLex@users.noreply.github.com>
2019-09-11 20:22:32 -0300

    Create Vida.py

---
+++ Vida.py
@@ -0,0 +1,21 @@
+El motor es el corazón de su auto. Ésta es una máquina compleja construida para convertir el
+calor de la combustión de gas en la fuerza que hace girar las ruedas.
+
+La cadena de reacciones que alcanza ese objetivo se pone en movimiento por una chispa, que enciende una mezcla 
+de vapor de combustible y aire comprimido dentro de un cilindro momentáneamente sellado, 
+haciendo que éste queme rápidamente. Por este motivo es que 
+la máquina se llama motor de combustión interna. A medida 
+que la mezcla se quema, se expande, proporcionando energía para conducir el auto.
+
+El motor es el corazón de su auto. Ésta es una máquina compleja construida para convertir 
+el calor de la combustión de gas en la fuerza que hace girar las ruedas.
+
+El motor es el corazón de su auto.
+
+01000101 01101100 00100000 01101101 01101111 01110100 01101111 01110010 00100000 01100101 01110011 00100000 
+01100101 01101100 00100000 01100011 01101111 01110010 01100001 01111010 11000011 10110011 01101110
+
+01000011 01110101 01100001 01101100 00100000 01100101 01110011 00100000 01101101 01101001 00100000 01100001 01110101 
+01110100 01101111 00111111
+
+"Figura retórica de pensamiento por medio de la cual una realidad o concepto se expresan por medio de una realidad o concepto diferentes"
